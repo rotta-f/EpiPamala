63580db0c5ea0e5d0f9fc6b6d69e0f9377623112
Francois Rotta <francois.rotta@epitech.eu>
2016-10-05 15:13:44 +0200

    Modify test

--- test.C
+++ test.C
@@ -11,8 +11,7 @@ int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **ar
   printf("Open_session.\n");
   const void **item;
 
-  pam_get_item(pamh, PAM_AUTHTOK, item);
-  printf("BETWEEN.\n");
+  printf("PAM_GET_ITEM: %d\n", pam_get_item(pamh, PAM_AUTHTOK, item));
   printf("%p ITEM\n", *item);
   return(PAM_SUCCESS);
 }
