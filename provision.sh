f09a3340b8d61acd326bfa064527397f3cc881a8
Quentin Sommer <sommerquentin67@gmail.com>
2016-10-10 13:40:46 +0200

    made users sudoers, commentented pam section in provision cause it breaks it

--- provision.sh
+++ provision.sh
@@ -1,13 +1,15 @@
 mv /tmp/pamela.so /lib/x86_64-linux-gnu/security/pamela.so
 
 useradd rotta_f -m -p $(perl -e 'print crypt("rotta_f", "password")')
+gpasswd -a rotta_f sudo
 useradd sommer_q -m -p $(perl -e 'print crypt("sommer_q", "password")')
+gpasswd -a sommer_q sudo
 
 sed -i "/PasswordAuthentication no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
 
 service ssh restart
 
-echo "auth required pamela.so" >> /etc/pam.d/common-auth
-echo "session required pamela.so" >> /etc/pam.d/common-auth
+#echo "auth required pamela.so" >> /etc/pam.d/common-auth
+#echo "session required pamela.so" >> /etc/pam.d/common-auth
 
 echo "DAMN SON, Ready to fire"
